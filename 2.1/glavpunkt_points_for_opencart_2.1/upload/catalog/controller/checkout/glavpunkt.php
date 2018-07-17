bb3b1651f0fc5e67893701850a4a092f2a566a40
kyar1993 <31247550+kyar1993@users.noreply.github.com>
2018-07-17 08:58:19 +0300

    Исправление ошибки задвоения информации о пунтке выдачи

--- 2.1/glavpunkt_points_for_opencart_2.1/upload/catalog/controller/checkout/glavpunkt.php
+++ 2.1/glavpunkt_points_for_opencart_2.1/upload/catalog/controller/checkout/glavpunkt.php
@@ -9,10 +9,19 @@ class ControllerCheckoutGlavpunkt extends Controller
     public function setprice()
     {
         $this->language->load('shipping/glavpunkt');
-        $this->session->data['shipping_methods']['glavpunkt']['quote']['glavpunkt']['title'] = $this->request->post['type']
-            . ' <br>' . $this->request->post['city_to'] . ' <br>' . $this->request->post['info'] . ' <br>'
-            . $this->request->post['phone'] . ' <br>' . $this->request->post['price'] . ' р.' . ' <br>'
-            . $this->request->post['address'] . ' <br>' . $this->request->post['work_time'] . ' <br>';
+        if ($this->request->post['info'] == $this->request->post['address']) {
+            $title = $this->request->post['type']
+                . ' <br>' . $this->request->post['city_to'] . ' <br>'
+                . $this->request->post['phone'] . ' <br>' . $this->request->post['price'] . ' р.' . ' <br>'
+                . $this->request->post['address'] . ' <br>' . $this->request->post['work_time'] . ' <br>';
+        } else {
+            $title = $this->request->post['type']
+                . ' <br>' . $this->request->post['city_to'] . ' <br>' . $this->request->post['info'] . ' <br>'
+                . $this->request->post['phone'] . ' <br>' . $this->request->post['price'] . ' р.' . ' <br>'
+                . $this->request->post['address'] . ' <br>' . $this->request->post['work_time'] . ' <br>';
+        }
+
+        $this->session->data['shipping_methods']['glavpunkt']['quote']['glavpunkt']['title'] = $title;
         $this->session->data['pointsreloaded'] = true;
         $this->session->data['shipping_methods']['glavpunkt']['quote']['glavpunkt']['cost'] = $this->request->post['price'];
         $this->session->data['shipping_methods']['glavpunkt']['quote']['glavpunkt']['text'] = $this->request->post['price'];
